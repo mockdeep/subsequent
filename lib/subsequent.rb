702f4fc9dbef22c77eec5bdd6a3e206013db9012
Robert Fletcher <lobatifricha@gmail.com>
2024-04-24 14:32:05 -0700

    Initial Commit!

---
+++ lib/subsequent.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "subsequent/version"
+
+module Subsequent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/subsequent/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Subsequent
+  VERSION = "0.1.0"
+end
