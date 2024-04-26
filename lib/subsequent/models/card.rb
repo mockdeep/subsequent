7f567f2d9212f4f63dfa8ff7988c9f125923ff0a
Robert Fletcher <lobatifricha@gmail.com>
2024-04-25 21:28:21 -0700

    fix when all checklist items are complete

--- lib/subsequent/models/card.rb
+++ lib/subsequent/models/card.rb
@@ -1,10 +1,11 @@
 class Subsequent::Models::Card
-  attr_accessor :checklists
+  attr_accessor :checklists, :name
 
-  def initialize(id:, **card_data)
+  def initialize(id:, name:, **card_data)
     self.checklists =
       Subsequent::TrelloClient.fetch_checklists(id).map do |checklist_data|
         Subsequent::Models::Checklist.new(card_id: id, **checklist_data)
       end
+    self.name = name
   end
 end
