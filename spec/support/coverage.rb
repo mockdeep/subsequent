e1a2bb72e2fcf72f2b3a60c583909349ca94ccd9
Robert Fletcher <lobatifricha@gmail.com>
2024-04-29 16:20:42 -0700

    add some boilerplate for testing/linting

---
+++ spec/support/coverage.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "simplecov"
+
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter "_spec.rb"
+  add_group "Lib", "lib"
+  add_group "Support", "spec/support"
+end
+
+SimpleCov.minimum_coverage(line: 100, branch: 100)
