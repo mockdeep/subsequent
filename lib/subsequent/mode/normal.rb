d63ae14a8ae8e805cacfdb5297611de2d63d7c34
Robert Fletcher <lobatifricha@gmail.com>
2024-05-26 15:22:40 -0700

    move normal input handling to mode (#42)

--- lib/subsequent/mode/normal.rb
+++ lib/subsequent/mode/normal.rb
@@ -3,6 +3,7 @@
 # Normal mode functionality
 module Subsequent::Mode::Normal
   extend Subsequent::DisplayHelpers
+  extend Subsequent::Configuration::Helpers
 
   # normal mode commands
   def self.commands(state)
@@ -27,5 +28,32 @@ module Subsequent::Mode::Normal
     item_range = (1..checklist_items.size).to_a.map(&method(:cyan))
 
     "(#{item_range.join(", ")}) toggle task"
+  end
+
+  # handle input for normal mode
+  def self.handle_input(state)
+    state => { checklist_items:, sort: }
+
+    char = input.getch
+
+    case char
+    when ("1"..checklist_items.to_a.size.to_s)
+      Subsequent::Commands::ToggleChecklistItem.call(state, char)
+    when "r"
+      show_spinner { Subsequent::Commands::FetchData.call(sort:) }
+    when "s"
+      Subsequent::State.new(**state.to_h, mode: :sort)
+    when "c"
+      Subsequent::State.new(**state.to_h, mode: :cycle)
+    when "o"
+      Subsequent::Commands::OpenLinks.call(state)
+    when "a"
+      Subsequent::Commands::ArchiveCard.call(state)
+    when "q", "\u0004", "\u0003"
+      output.puts yellow("Goodbye!")
+      exit
+    else
+      state
+    end
   end
 end
