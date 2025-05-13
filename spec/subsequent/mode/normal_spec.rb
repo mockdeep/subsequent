a50bb886d58d62284e7ca2227bec87bf5231e2ae
Robert Fletcher <lobatifricha@gmail.com>
2025-05-13 15:23:33 -0700

    add some missing test coverage

--- spec/subsequent/mode/normal_spec.rb
+++ spec/subsequent/mode/normal_spec.rb
@@ -2,6 +2,16 @@
 
 RSpec.describe Subsequent::Mode::Normal do
   describe ".handle_input" do
+    it "refreshes the data when input is r" do
+      state = make_state
+      mock_input("r")
+      stub_request(:get, /cards/).to_return(body: [api_card].to_json)
+
+      described_class.handle_input(state)
+
+      expect(a_request(:get, /cards/)).to have_been_made.once
+    end
+
     it "returns the state with filter mode when input is f" do
       state = make_state
       mock_input("f")
--- spec/subsequent/models/checklist_spec.rb
+++ spec/subsequent/models/checklist_spec.rb
@@ -15,4 +15,27 @@ RSpec.describe Subsequent::Models::Checklist do
       expect(checklist.items.map(&:pos)).to eq([31, 52])
     end
   end
+
+  describe "#<=>" do
+    it "compares the position of two checklists" do
+      checklist1 = make_checklist(pos: 2)
+      checklist2 = make_checklist(pos: 1)
+
+      expect([checklist1, checklist2].sort).to eq([checklist2, checklist1])
+    end
+  end
+
+  describe "#tags" do
+    it "returns the tags from the checklist name" do
+      checklist = make_checklist(name: "foo @bar baz @qux")
+
+      expect(checklist.tags).to eq(["@bar", "@qux"])
+    end
+
+    it "returns <no tag> if no tags are present" do
+      checklist = make_checklist(name: "foo bar baz")
+
+      expect(checklist.tags).to eq(["<no tag>"])
+    end
+  end
 end
