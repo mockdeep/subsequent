6390beef958dd0cbda606234756d68444e1fddbf
Robert Fletcher <lobatifricha@gmail.com>
2025-05-15 16:36:57 -0700

    add rspec random and focus (#117)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,11 +12,16 @@ require_relative "support/trello"
 require_relative "support/webmock"
 
 RSpec.configure do |config|
+  config.order = :random
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.filter_run(focus: true)
+  config.run_all_when_everything_filtered = true
 
   config.expect_with(:rspec) do |c|
     c.syntax = :expect
