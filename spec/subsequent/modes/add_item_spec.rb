e6479a19e22d33f1090a0064ab7737e362203482
Robert Fletcher <lobatifricha@gmail.com>
2025-05-16 17:30:57 -0700

    stop passing args in State

--- spec/subsequent/modes/add_item_spec.rb
+++ spec/subsequent/modes/add_item_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Subsequent::Modes::AddItem do
     end
 
     it "returns state with mode AddChecklistItem when input is i" do
-      state = make_state(checklist: make_checklist)
+      state = make_state(cards: [make_card_with_item])
       input.print("i")
       input.rewind
 
