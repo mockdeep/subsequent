6a3fa5a56fc4526bcaa6bc589a57e1b2f93d7781
Robert Fletcher <lobatifricha@gmail.com>
2025-05-16 17:32:49 -0700

    convert state to class

--- lib/subsequent/state.rb
+++ lib/subsequent/state.rb
@@ -9,25 +9,20 @@ Subsequent::State =
     :filter,
     :mode,
     :sort,
-  ) do
-    def initialize(cards:, sort:, filter:, **args)
-      mode = args.fetch(:mode) { Subsequent::Modes::Normal }
-      cards = filter.call(cards)
-      card = sort.call(cards) || Subsequent::Models::NullCard.new
-      checklist =
-        args.fetch(:checklist) { card.checklists.find(&:unchecked_items?) }
-      checklist ||= Subsequent::Models::NullChecklist.new
-      checklist_items =
-        args.fetch(:checklist_items) { checklist.unchecked_items.first(5) }
+  )
 
-      super(
-        cards:,
-        filter:,
-        sort:,
-        card:,
-        checklist:,
-        checklist_items:,
-        mode:,
-      )
-    end
+# class to handle state of the application
+class Subsequent::State
+  def initialize(cards:, sort:, filter:, **args)
+    mode = args.fetch(:mode) { Subsequent::Modes::Normal }
+    cards = filter.call(cards)
+    card = sort.call(cards) || Subsequent::Models::NullCard.new
+    checklist =
+      args.fetch(:checklist) { card.checklists.find(&:unchecked_items?) }
+    checklist ||= Subsequent::Models::NullChecklist.new
+    checklist_items =
+      args.fetch(:checklist_items) { checklist.unchecked_items.first(5) }
+
+    super(cards:, filter:, sort:, card:, checklist:, checklist_items:, mode:)
   end
+end
