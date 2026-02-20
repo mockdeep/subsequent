b5422f373ef6a9d3217bc91a3628a3fe4fbdcc86
Robert Fletcher <lobatifricha@gmail.com>
2026-02-20 09:34:53 -0800

    preserve browse state across refreshes

--- lib/subsequent/options/refresh.rb
+++ lib/subsequent/options/refresh.rb
@@ -16,11 +16,36 @@ module Subsequent::Options::Refresh
     def call(state, _text)
       state => { filter:, sort:, browse_list_id:, lists: }
 
-      show_spinner do
-        Subsequent::Commands::FetchData.call(
-          filter:, sort:, list_id: browse_list_id, lists:,
-        )
-      end
+      new_state =
+        show_spinner do
+          Subsequent::Commands::FetchData.call(
+            filter:, sort:, list_id: browse_list_id, lists:,
+          )
+        end
+
+      restore_selection(new_state, state.card, state.checklist)
+    end
+
+    private
+
+    def restore_selection(new_state, old_card, old_checklist)
+      card = find_by_id(new_state.cards, old_card)
+      return new_state unless card
+
+      checklist = find_by_id(card.checklists, old_checklist)
+      return new_state.with(card:) unless checklist
+
+      new_state.with(
+        card:,
+        checklist:,
+        checklist_items: checklist.unchecked_items.first(5),
+      )
+    end
+
+    def find_by_id(collection, old_item)
+      return unless old_item.respond_to?(:id)
+
+      collection.find { |item| item.id == old_item.id }
     end
   end
 end
